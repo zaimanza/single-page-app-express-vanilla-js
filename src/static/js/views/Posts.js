1b5c223886350a09c1f9c9eb89e8c00a95f9c8ce
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-12 00:57:50 +0800

    init

--- src/static/js/views/Posts.js
+++ src/static/js/views/Posts.js
@@ -8,6 +8,11 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
+         <nav class="nav">
+         <a href="/" class="nav__link" data-link>Dashboard</a>
+         <a href="/posts" class="nav__link" data-link>Posts</a>
+         <a href="/settings" class="nav__link" data-link>Settings</a>
+         </nav>
             <h1>Posts</h1>
             <p>You are viewing the posts!</p>
         `;
